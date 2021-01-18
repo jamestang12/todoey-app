6ca64bc4c01162debc44c579dc3edb62c0799023
jamestang12 <homing.tang@ryerson.ca>
2021-01-18 17:21:50 -0500

    first commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by James Tang on 01/18/2020.
+//  Copyright (c) James Tang. All rights reserved.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
