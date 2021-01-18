3b8147266e107cfdd7eb1dbbdd6d47841b429004
jamestang12 <homing.tang@ryerson.ca>
2021-01-18 17:42:06 -0500

    Table View Created

---
+++ Todoey/TodoListViewControl.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by James Tang on 01/18/2020.
+//  Copyright (c) James Tang. All rights reserved.
+//
+
+
+import UIKit
+
+class TodoListViewControl: UITableViewController {
+
+    let itemArray = ["Find Mike", "Buy Eggos", "Destory Demogorgon"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+
+}
+
