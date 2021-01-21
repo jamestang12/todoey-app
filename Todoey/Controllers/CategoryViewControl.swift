4c7ddc3a7e39e71c6c3da1b8cb4c815c589d8ae8
jamestang12 <homing.tang@ryerson.ca>
2021-01-21 15:06:08 -0500

    Set Up Category Controller

---
+++ Todoey/Controllers/CategoryViewControl.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryViewCell.swift
+//  Todoey
+//
+//  Created by James Tang on 2021-01-21.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CategoryViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
