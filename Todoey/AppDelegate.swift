e2fbf24fae0a64845b9a89e25e9005605927961f
jamestang12 <homing.tang@ryerson.ca>
2021-01-21 22:36:39 -0500

    Config Realm

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,7 +19,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)
+        print(Realm.Configuration.defaultConfiguration.fileURL)
+        
+        let data = Data()
+        data.name = "James"
+        data.age = 20
+        
+        do{
+            let realm = try Realm()
+            try realm.write{
+                realm.add(data)
+            }
+            
+        }catch{
+            print("Error initialisting new realm, \(error)")
+        }
+        
+        
         
         return true
     }
